96be5ce7f6dddb886e178ba6519cf715257d4a38
Riccio <riccio.fernando@gmail.com>
2025-10-13 20:09:31 -0300

    imagens do footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,27 @@
+import instagramImage from '../../assets/instagram.svg'
+import facebookImage from '../../assets/facebook.svg'
+import twitterImage from '../../assets/twitter.svg'
+import youtubeImage from '../../assets/youtube.svg'
+import linkedinImage from '../../assets/linkedin.svg'
+
 export default function Footer() {
     return (
         <footer>
         <nav className="socials" aria-label="Redes sociais">
             <a href="#" className="social_link" aria-label="Link para o Instagram">
-                <img src="./assets/instagram.svg" alt="Instagram" className="social_icon"/>
+                <img src={instagramImage} alt="Instagram" className="social_icon"/>
             </a>
             <a href="#" className="social_link" aria-label="Link para o Facebook">
-                <img src="./assets/facebook.svg" alt="Facebook" className="social_icon"/>
+                <img src={facebookImage} alt="Facebook" className="social_icon"/>
             </a>
             <a href="#" className="social_link" aria-label="Link para o Twitter">
-                <img src="./assets/twitter.svg" alt="Twitter" className="social_icon"/>
+                <img src={twitterImage} alt="Twitter" className="social_icon"/>
             </a>
             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="social_link" aria-label="Link para o YouTube">
-                <img src="./assets/youtube.svg" alt="YouTube" className="social_icon"/>
+                <img src={youtubeImage} alt="YouTube" className="social_icon"/>
             </a>
             <a href="#" className="social_link" aria-label="Link para o LinkedIn">
-                <img src="./assets/linkedin.svg" alt="LinkedIn" className="social_icon"/>
+                <img src={linkedinImage} alt="LinkedIn" className="social_icon"/>
             </a>
         </nav>
         <p className="copyright">&copy; 2025 6G Mackenzie. Todos os direitos reservados.</p>
