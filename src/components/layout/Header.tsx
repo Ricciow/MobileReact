85d8ef1c0bd2de506c81e77ca428e8d026bcb9e0
Riccio <riccio.fernando@gmail.com>
2025-10-13 20:13:20 -0300

    ajustar header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,27 @@
+import imagemMenu from '../../assets/menu.svg'
+import imagemClose from '../../assets/close.svg'
+import imagemSearch from '../../assets/search.svg'
+import imagemUser from '../../assets/user.png'
+import { Link } from 'react-router-dom'
+
 export default function Header() {
     return(
         <header>
-            <h1 className="logo"><a href="./index.html" aria-label="Ir para a home" className="logo_link">HABIT</a></h1>
+            <h1 className="logo"><Link to="./index.html" aria-label="Ir para a home" className="logo_link">HABIT</Link></h1>
 
             <nav className="header_nav" aria-label="Menu principal">
-                <a href="./newsletter.html" className="header_link">Assinar</a>
+                <Link to="./newsletter.html" className="header_link">Assinar</Link>
                 <button className="menu_button" aria-label="Abrir Menu" aria-expanded="false" aria-controls="header_menu" id="menu_hamburguer">
-                    <img src="./assets/menu.svg" alt="menu" className="menu_icon"/>
-                    <img src="./assets/close.svg" alt="fechar_menu" className="close_menu_icon"/>
+                    <img src={imagemMenu} alt="menu" className="menu_icon"/>
+                    <img src={imagemClose} alt="fechar_menu" className="close_menu_icon"/>
                 </button>
                 <ul id="header_menu" className="header_menu">
-                    <li><a href="./index.html" className="header_link">Início</a></li>
-                    <li><a href="#" className="header_link">Páginas</a></li>
-                    <li><a href="./categories.html?category=Destaques" className="header_link">Destaques</a></li>
-                    <li><a href="./administrator-page-category.html" className="header_link">Admin</a></li>
-                    <li><a href="./login.html" className="header_link profile_link_menu">Perfil</a></li>
-                    <li><a href="./posts.html" className="header_link">Posts</a></li>
+                    <li><Link to="./index.html" className="header_link">Início</Link></li>
+                    <li><Link to="#" className="header_link">Páginas</Link></li>
+                    <li><Link to="./categories.html?category=Destaques" className="header_link">Destaques</Link></li>
+                    <li><Link to="./administrator-page-category.html" className="header_link">Admin</Link></li>
+                    <li><Link to="./login.html" className="header_link profile_link_menu">Perfil</Link></li>
+                    <li><Link to="./posts.html" className="header_link">Posts</Link></li>
                 </ul>
             </nav>
 
@@ -23,14 +29,14 @@ export default function Header() {
                 <form  action="./busca.html" className="search card" method="GET" role="search" aria-label="Pesquisar no site">
                     <input type="text" placeholder="Pesquisar..." name="q" className="input search_input" />
                     <button type="submit" className="search_button" aria-label="Buscar" >
-                        <img src="./assets/search.svg" alt="buscar" className="search_icon"/>
+                        <img src={imagemSearch} alt="buscar" className="search_icon"/>
                     </button>
                 </form>
             </div>
 
-            <a href="./login.html" aria-label="Acessar seu perfil" className="profile_link">
-                <img src="./assets/user.png" alt="Foto do perfil"/>
-            </a>
+            <Link to="./login.html" aria-label="Acessar seu perfil" className="profile_link">
+                <img src={imagemUser} alt="Foto do perfil"/>
+            </Link>
         </header>
     )
 }
